40c8b1e0c69a1176be3350926adfd11254b160ca
Giovanni Ferron (gvnn) <gvnnrulez@gmail.com>
2014-07-30 22:31:26 +1000

    Add destination class
    Created class for generating a destination html template

---
+++ lib/destination.rb
@@ -0,0 +1,42 @@
+require 'erb'
+require 'xml'
+
+class Destination
+  attr_reader :content
+  attr_reader :destination
+
+  def initialize(id, taxonomy, erb_renderer, xslt_stylesheet)
+    @id = id
+    @erb_renderer = erb_renderer
+    @xslt_stylesheet = xslt_stylesheet
+    @taxonomy = taxonomy
+  end
+
+  def parse_xml(xml)
+    begin
+      @content = @xslt_stylesheet.apply(XML::Parser.string(xml).parse)
+      true
+    rescue
+      false
+    end
+  end
+
+  def render_template
+    @erb_renderer.result(binding)
+  end
+
+  def create_html(output_folder)
+
+    @destination = {
+        :details => @taxonomy.get(@id),
+        :parent => @taxonomy.parent(@id),
+        :children => @taxonomy.get_children(@id)
+    }
+
+    File.open(File.join(output_folder, "#{@id}.html"), "w") do |f|
+      f << render_template
+    end
+
+  end
+
+end
